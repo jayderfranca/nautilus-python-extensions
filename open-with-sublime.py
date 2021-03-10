2670b495938898ba6dcba24f15ba6ec1d4adf941
Jayder França <jayderfranca@gmail.com>
2021-03-10 14:16:06 -0300

    Novo Open With - Sublime Text

---
+++ open-with-sublime.py
@@ -0,0 +1,53 @@
+import os
+from gi.repository import Nautilus, GObject
+
+
+class OpenWithSublimeExtension(GObject.GObject, Nautilus.MenuProvider):
+
+    def __init__(self, *args, **kwargs):
+        super(OpenWithSublimeExtension, self).__init__(*args, **kwargs)
+        self._location = "/opt/sublime_text/sublime_text"
+
+    def _is_installed(self):
+        return self._location is not None
+
+    def _open_sublime(self, item):
+        os.system('%s %s' % (self._location, item.get_location().get_path()))
+
+    def menu_activate_cb(self, _, item):
+        self._open_sublime(item)
+
+    def menu_background_activate_cb(self, _, current_folder):
+        self._open_sublime(current_folder)
+
+    def get_file_items(self, _, files):
+
+        if not self._is_installed():
+            return
+
+        if len(files) != 1:
+            return
+
+        selected = files[0]
+        if selected.get_uri_scheme() != 'file':
+            return
+
+        print("get_file_items")
+        print(selected)
+
+        item = Nautilus.MenuItem(name='Nautilus::open_with_sublime_file',
+                                 label='Open With Sublime')
+        item.connect('activate', self.menu_activate_cb, selected)
+        return item,
+
+    def get_background_items(self, _, current_folder):
+        if current_folder.get_uri_scheme() != 'file':
+            return
+
+        print("get_background_items")
+        print(current_folder)
+
+        item = Nautilus.MenuItem(name='Nautilus::open_with_sublime_background',
+                                 label='Open With Sublime')
+        item.connect('activate', self.menu_background_activate_cb, current_folder)
+        return item,
