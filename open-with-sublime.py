9ef4016e4f475d165aa186dbf6ffe6058f23b75d
Jayder França <jayderfranca@gmail.com>
2022-04-21 01:11:29 -0300

    Redução na quantidade de funções

--- open-with-sublime.py
+++ open-with-sublime.py
@@ -14,7 +14,10 @@ class OpenWithSublimeExtension(GObject.GObject, Nautilus.MenuProvider):
     def _open_sublime(self, item):
         os.system('%s %s' % (self._location, item.get_location().get_path()))
 
-    def _new_menu_nautilus(self, name, file, callback):
+    def menu_activate_cb(self, _, item):
+        self._open_sublime(item)
+
+    def _new_menu_nautilus(self, name, file):
 
         if not self._is_installed():
             return
@@ -25,18 +28,12 @@ class OpenWithSublimeExtension(GObject.GObject, Nautilus.MenuProvider):
         item = Nautilus.MenuItem(name='OpenWithSublimeExtension::' + name,
                                  label='Open With Sublime')
 
-        item.connect('activate', callback, file)
+        item.connect('activate', self.menu_activate_cb, file)
         return item,
-
-    def menu_activate_cb(self, _, item):
-        self._open_sublime(item)
-
-    def menu_background_activate_cb(self, _, current_folder):
-        self._open_sublime(current_folder)
 
     def get_file_items(self, _, files):
         if len(files) == 1:
-            return self._new_menu_nautilus("File", files[0], self.menu_activate_cb)
+            return self._new_menu_nautilus("File", files[0])
 
     def get_background_items(self, _, current_folder):
-        return self._new_menu_nautilus("Background", current_folder, self.menu_background_activate_cb)
+        return self._new_menu_nautilus("Background", current_folder)
